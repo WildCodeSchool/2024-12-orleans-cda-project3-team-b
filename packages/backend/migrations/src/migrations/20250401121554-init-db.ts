6c3852d23ddf45dcb603ce79178ff46caa2c219a
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-04-04 11:30:45 +0200

    update

--- packages/backend/migrations/src/migrations/20250401121554-init-db.ts
+++ packages/backend/migrations/src/migrations/20250401121554-init-db.ts
@@ -5,38 +5,38 @@ import type { DB } from '@app/shared';
 export async function up(db: Kysely<DB>): Promise<void> {
   // Migration code that update the database to the desired state.
   await db.transaction().execute(async (trx) => {
-    await sql`CREATE TABLE level (
+    await sql`CREATE TABLE levels (
       id INT AUTO_INCREMENT PRIMARY KEY,
       value INT NOT NULL
   );`.execute(trx);
 
-    await sql`CREATE TABLE logo (
+    await sql`CREATE TABLE logos (
       id INT AUTO_INCREMENT PRIMARY KEY,
       logo_img VARCHAR(255) NOT NULL
   );`.execute(trx);
 
     await sql`
-    CREATE TABLE label (
+    CREATE TABLE labels (
         id INT AUTO_INCREMENT PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
-        logo_id INT NOT NULL,
+        logos_id INT NOT NULL,
         budget FLOAT NOT NULL,
         notoriety FLOAT NOT NULL,
-        level_id INT  NOT NULL,
+        levels_id INT  NOT NULL,
         score_xp INT NOT NULL,
-        FOREIGN KEY (logo_id) REFERENCES logo(id),
-        FOREIGN KEY (level_id) REFERENCES level(id)
+        FOREIGN KEY (logos_id) REFERENCES logos(id),
+        FOREIGN KEY (levels_id) REFERENCES levels(id)
     );`.execute(trx);
 
     await sql`
-CREATE TABLE user (
+CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     is_first_time BOOLEAN NOT NULL,
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    label_id INT  NOT NULL,
-    FOREIGN KEY (label_id) REFERENCES label(id)
+    labels_id INT  NOT NULL,
+    FOREIGN KEY (labels_id) REFERENCES labels(id)
 );`.execute(trx);
 
     await sql`
@@ -47,65 +47,65 @@ CREATE TABLE milestones (
 );
 `.execute(trx);
 
-    await sql`CREATE TABLE skill (
+    await sql`CREATE TABLE skills (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     score INT NOT NULL,
     exp_value INT  NOT NULL
 );`.execute(trx);
 
-    await sql`CREATE TABLE genre (
+    await sql`CREATE TABLE genres (
   id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(255) NOT NULL
 );`.execute(trx);
-    await sql`CREATE TABLE artist (
+    await sql`CREATE TABLE artists (
     id INT AUTO_INCREMENT PRIMARY KEY,
     firstname VARCHAR(255) DEFAULT NULL,
     lastname VARCHAR(255) DEFAULT NULL,
     alias VARCHAR(255) DEFAULT NULL,
     notoriety FLOAT NOT NULL,
-    genre_id INT NOT NULL,
-    milestone_id INT NOT NULL,
+    genres_id INT NOT NULL,
+    milestones_id INT NOT NULL,
     price INT NOT NULL,
     exp_value INT NOT NULL,
-    FOREIGN KEY (genre_id) REFERENCES genre(id),
-    FOREIGN KEY (milestone_id) REFERENCES milestones(id)
-);`.execute(trx);
-
-    await sql`CREATE TABLE artist_hired (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    artist_id INT NOT NULL,
-    skill_id INT NOT NULL,
-    notoriety FLOAT NOT NULL,
-    milestones_id INT NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES artist(id),
-    FOREIGN KEY (skill_id) REFERENCES skill(id),
+    FOREIGN KEY (genres_id) REFERENCES genres(id),
     FOREIGN KEY (milestones_id) REFERENCES milestones(id)
 );`.execute(trx);
 
-    await sql`CREATE TABLE single (
+    await sql`CREATE TABLE artists_hired (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    artist_hired_id INT NOT NULL,
-    genre_id INT NOT NULL,
-    note FLOAT NOT NULL,
-    listener INT NOT NULL,
-    money_earned INT NOT NULL,
-    exp_value INT NOT NULL,
-    FOREIGN KEY (artist_hired_id) REFERENCES artist_hired(id),
-    FOREIGN KEY (genre_id) REFERENCES genre(id)
+    artists_id INT NOT NULL,
+    skills_id INT NOT NULL,
+    notoriety FLOAT NOT NULL,
+    milestones_id INT NOT NULL,
+    FOREIGN KEY (artists_id) REFERENCES artists(id),
+    FOREIGN KEY (skills_id) REFERENCES skills(id),
+    FOREIGN KEY (milestones_id) REFERENCES milestones(id)
 );`.execute(trx);
 
-    await sql`CREATE TABLE album (
+    await sql`CREATE TABLE singles (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    artists_hired_id INT NOT NULL,
+    genres_id INT NOT NULL,
+    note FLOAT NOT NULL,
+    listeners INT NOT NULL,
+    money_earned INT NOT NULL,
+    exp_value INT NOT NULL,
+    FOREIGN KEY (artists_hired_id) REFERENCES artists_hired(id),
+    FOREIGN KEY (genres_id) REFERENCES genres(id)
+);`.execute(trx);
+
+    await sql`CREATE TABLE albums (
     id INT AUTO_INCREMENT PRIMARY KEY,
     score FLOAT NOT NULL,
-    buyer INT NOT NULL,
+    sales INT NOT NULL,
     money_earned INT NOT NULL,
-    genre_id INT NOT NULL,
+    genres_id INT NOT NULL,
     exp_value INT NOT NULL,
-    artist_id INT NOT NULL,
-    FOREIGN KEY (genre_id) REFERENCES genre(id),
-    FOREIGN KEY (artist_id) REFERENCES artist(id)
+    artists_id INT NOT NULL,
+    FOREIGN KEY (genres_id) REFERENCES genres(id),
+    FOREIGN KEY (artists_id) REFERENCES artists(id)
 );`.execute(trx);
 
     await sql`CREATE TABLE staff (
@@ -116,7 +116,7 @@ CREATE TABLE milestones (
     exp_value INT NOT NULL
 );`.execute(trx);
 
-    await sql`CREATE TABLE entourage (
+    await sql`CREATE TABLE crew_members (
     id INT AUTO_INCREMENT PRIMARY KEY,
     job VARCHAR(255) NOT NULL,
     bonus INT NOT NULL,
@@ -132,71 +132,71 @@ CREATE TABLE milestones (
     exp_value INT NOT NULL
 );`.execute(trx);
 
-    await sql`CREATE TABLE artist_hired_skill (
+    await sql`CREATE TABLE artists_hired_skills (
   id INT AUTO_INCREMENT PRIMARY KEY,
-  artist_hired_id INT NOT NULL,
-  skill_id INT NOT NULL,
+  artists_hired_id INT NOT NULL,
+  skills_id INT NOT NULL,
   score INT NOT NULL,
-  FOREIGN KEY (artist_hired_id) REFERENCES artist_hired(id),
-  FOREIGN KEY (skill_id) REFERENCES skill(id)
+  FOREIGN KEY (artists_hired_id) REFERENCES artists_hired(id),
+  FOREIGN KEY (skills_id) REFERENCES skills(id)
 );`.execute(trx);
 
-    await sql`CREATE TABLE artist_skill (
+    await sql`CREATE TABLE artists_skills (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    artist_hired_id INT NOT NULL,
-    skill_id INT NOT NULL,
+    artists_hired_id INT NOT NULL,
+    skills_id INT NOT NULL,
     score INT NOT NULL,
-    FOREIGN KEY (artist_hired_id) REFERENCES artist_hired(id),
-    FOREIGN KEY (skill_id) REFERENCES skill(id)
+    FOREIGN KEY (artists_hired_id) REFERENCES artists_hired(id),
+    FOREIGN KEY (skills_id) REFERENCES skills(id)
 );`.execute(trx);
 
-    await sql`CREATE TABLE entourage_hired (
+    await sql`CREATE TABLE crew_members_hired (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    entourage_id INT NOT NULL,
-    artist_id INT NOT NULL,
-    FOREIGN KEY (artist_id) REFERENCES artist(id)
+    crew_members_id INT NOT NULL,
+    artists_id INT NOT NULL,
+    FOREIGN KEY (artists_id) REFERENCES artists(id)
 );`.execute(trx);
 
     await sql`CREATE TABLE staff_label (
     id INT AUTO_INCREMENT PRIMARY KEY,
     staff_id INT NOT NULL,
-    label_id INT NOT NULL,
+    labels_id INT NOT NULL,
     FOREIGN KEY (staff_id) REFERENCES staff(id),
-    FOREIGN KEY (label_id) REFERENCES label(id)
+    FOREIGN KEY (labels_id) REFERENCES labels(id)
 );`.execute(trx);
 
-    await sql`CREATE TABLE label_artist (
+    await sql`CREATE TABLE label_artists (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    label_id INT NOT NULL,
-    artist_hired_id INT NOT NULL,
-    FOREIGN KEY (label_id) REFERENCES label(id),
-    FOREIGN KEY (artist_hired_id) REFERENCES artist_hired(id)
+    labels_id INT NOT NULL,
+    artists_hired_id INT NOT NULL,
+    FOREIGN KEY (labels_id) REFERENCES labels(id),
+    FOREIGN KEY (artists_hired_id) REFERENCES artists_hired(id)
 );`.execute(trx);
 
-    await sql`CREATE TABLE single_album (
+    await sql`CREATE TABLE singles_albums (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    single_id INT NOT NULL,
-    album_id INT NOT NULL,
-    FOREIGN KEY (single_id) REFERENCES single(id),
-    FOREIGN KEY (album_id) REFERENCES album(id)
+    singles_id INT NOT NULL,
+    albums_id INT NOT NULL,
+    FOREIGN KEY (singles_id) REFERENCES singles(id),
+    FOREIGN KEY (albums_id) REFERENCES albums(id)
 );`.execute(trx);
 
-    await sql`CREATE TABLE artist_marketing (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    marketing_id INT NOT NULL,
-    single_id INT NOT NULL,
-    album_id INT NOT NULL,
-    FOREIGN KEY (marketing_id) REFERENCES marketing(id),
-    FOREIGN KEY (single_id) REFERENCES single(id),
-    FOREIGN KEY (album_id) REFERENCES album(id)
-);`.execute(trx);
-
-    await sql`CREATE TABLE album_marketing (
+    await sql`CREATE TABLE artists_marketing (
     id INT AUTO_INCREMENT PRIMARY KEY,
     marketing_id INT NOT NULL,
-    album_id INT NOT NULL,
+    singles_id INT NOT NULL,
+    albums_id INT NOT NULL,
     FOREIGN KEY (marketing_id) REFERENCES marketing(id),
-    FOREIGN KEY (album_id) REFERENCES album(id)
+    FOREIGN KEY (singles_id) REFERENCES singles(id),
+    FOREIGN KEY (albums_id) REFERENCES albums(id)
+);`.execute(trx);
+
+    await sql`CREATE TABLE albums_marketing (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    marketing_id INT NOT NULL,
+    albums_id INT NOT NULL,
+    FOREIGN KEY (marketing_id) REFERENCES marketing(id),
+    FOREIGN KEY (albums_id) REFERENCES albums(id)
 );`.execute(trx);
   });
 }
@@ -205,70 +205,70 @@ export async function down(db: Kysely<DB>): Promise<void> {
   // Migration code that reverts the database to the previous state.
   await db.transaction().execute(async (trx) => {
     await sql`
-      DROP TABLE IF EXISTS album_marketing;
+      DROP TABLE IF EXISTS albums_marketing;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS artist_marketing;
+      DROP TABLE IF EXISTS artists_marketing;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS single_album;
+      DROP TABLE IF EXISTS singles_albums;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS label_artist;
+      DROP TABLE IF EXISTS label_artists;
     `.execute(trx);
     await sql`
       DROP TABLE IF EXISTS staff_label;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS entourage_hired;
+      DROP TABLE IF EXISTS crew_members_hired;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS  artist_skill;
+      DROP TABLE IF EXISTS  artists_skill;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS artist_hired_skill;
+      DROP TABLE IF EXISTS artists_hired_skill;
     `.execute(trx);
     await sql`
       DROP TABLE IF EXISTS marketing ;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS entourage;
+      DROP TABLE IF EXISTS crew_members;
     `.execute(trx);
     await sql`
       DROP TABLE IF EXISTS staff;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS album;
+      DROP TABLE IF EXISTS albums;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS single;
+      DROP TABLE IF EXISTS singles;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS artist_hired;
+      DROP TABLE IF EXISTS artists_hired;
     `.execute(trx);
     await sql`
       DROP TABLE IF EXISTS artists;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS genre;
+      DROP TABLE IF EXISTS genres;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS skill;
+      DROP TABLE IF EXISTS skills;
     `.execute(trx);
     await sql`
       DROP TABLE IF EXISTS milestones;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS user;
+      DROP TABLE IF EXISTS users;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS label;
+      DROP TABLE IF EXISTS labels;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS logo;
+      DROP TABLE IF EXISTS logos;
     `.execute(trx);
     await sql`
-      DROP TABLE IF EXISTS level;
+      DROP TABLE IF EXISTS levels;
     `.execute(trx);
   });
 }
