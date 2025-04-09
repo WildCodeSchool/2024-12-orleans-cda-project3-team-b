b63c7bce480d797b68e5fe4d1b06f42b0c53d9b9
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-04-09 12:18:38 +0200

    visuel page MainMenu

---
+++ packages/frontend/web/src/pages/main-menu.tsx
@@ -0,0 +1,102 @@
+import { useNavigate } from 'react-router-dom';
+
+export default function MainMenu() {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    void navigate('/hireartist');
+  };
+
+  return (
+    <>
+      <header className='h-19 text-center text-4xl'>
+        <h1>{'HEADER'}</h1>
+      </header>
+      <div className='bg-[#EDF4F6] text-center'>
+        <div className='flex h-70 flex-col items-center justify-center'>
+          <h1 className='pb-7 text-6xl text-[#15414E] underline'>
+            {'MyArtists'}
+          </h1>
+          <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+            <button
+              type='button'
+              onClick={handleClick}
+              className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
+            >
+              {'+'}
+            </button>
+          </span>
+
+          <h2 className='pt-1 text-2xl text-[#15414E]'>
+            {'Hire a new artist'}
+          </h2>
+        </div>
+        <div className='h-70 pt-7'>
+          <h1 className='text-6xl text-[#15414E] underline'>{' Record'}</h1>
+
+          <div className='flex h-50 items-center justify-center'>
+            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+              <button
+                type='button'
+                onClick={handleClick}
+                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
+              >
+                {'+'}
+              </button>
+            </span>
+            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+              <button
+                type='button'
+                onClick={handleClick}
+                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
+              >
+                {'+'}
+              </button>
+            </span>
+          </div>
+        </div>
+        <div className='h-70 pt-7'>
+          <h1 className='text-6xl text-[#15414E] underline'>{' Staff'}</h1>
+          <div className='flex h-50 items-center justify-center'>
+            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+              <button
+                type='button'
+                onClick={handleClick}
+                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
+              >
+                {'+'}
+              </button>
+            </span>
+            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+              <button
+                type='button'
+                onClick={handleClick}
+                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
+              >
+                {'+'}
+              </button>
+            </span>
+            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+              <button
+                type='button'
+                onClick={handleClick}
+                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
+              >
+                {'+'}
+              </button>
+            </span>
+            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+              <button
+                type='button'
+                onClick={handleClick}
+                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
+              >
+                {'+'}
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
--- packages/frontend/web/src/router.tsx
+++ packages/frontend/web/src/router.tsx
@@ -1,11 +1,21 @@
 import { createBrowserRouter } from 'react-router-dom';
 
+import HireArtist from './pages/hire-artist.tsx';
 import Home from './pages/home';
+import MainMenu from './pages/main-menu';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <Home />,
+  },
+  {
+    path: '/mainmenu',
+    element: <MainMenu />,
+  },
+  {
+    path: '/hireartist',
+    element: <HireArtist />,
   },
 ]);
 
