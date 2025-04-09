dc7a8438a7f29a38ad55ea3a5e0e9ddddbd702db
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-04-09 15:32:21 +0200

    création component bouton

--- packages/frontend/web/src/pages/main-menu.tsx
+++ packages/frontend/web/src/pages/main-menu.tsx
@@ -8,11 +8,11 @@ export default function MainMenu() {
       <header className='h-19 text-center text-4xl'>
         <h1>{'HEADER'}</h1>
       </header>
-      <div className='bg-[#EDF4F6] text-center'>
+      <div className='mx-auto bg-[#EDF4F6] text-center'>
         <div className='flex h-70 flex-col items-center justify-center'>
-          <h1 className='pb-7 text-6xl text-[#15414E] underline'>
-            {'MyArtists'}
-          </h1>
+          <h2 className='pb-7 text-6xl text-[#15414E] underline'>
+            {'MyARTISTS'}
+          </h2>
           <Link to='/hireartist'>
             <Button>{'+'}</Button>
           </Link>
@@ -22,7 +22,7 @@ export default function MainMenu() {
           </h2>
         </div>
         <div className='h-70 pt-7'>
-          <h1 className='text-6xl text-[#15414E] underline'>{' Record'}</h1>
+          <h2 className='text-6xl text-[#15414E] underline'>{' RECORD'}</h2>
 
           <div className='flex h-50 items-center justify-center'>
             <div className='flex flex-col items-center justify-center'>
@@ -33,19 +33,31 @@ export default function MainMenu() {
             </div>
             <div className='flex flex-col items-center justify-center pl-10'>
               <Button>{'+'}</Button>
-              <h2 className='pt-1 text-2xl text-[#15414E]'>
+              <h3 className='pt-1 text-2xl text-[#15414E]'>
                 {'Create a new album'}
-              </h2>
+              </h3>
             </div>
           </div>
         </div>
         <div className='h-70 pt-7'>
-          <h1 className='text-6xl text-[#15414E] underline'>{' Staff'}</h1>
+          <h2 className='text-6xl text-[#15414E] underline'>{' STAFF'}</h2>
           <div className='flex h-50 items-center justify-center'>
-            <Button>{'+'}</Button>
-            <Button>{'+'}</Button>
-            <Button>{'+'}</Button>
-            <Button>{'+'}</Button>
+            <div className='flex flex-col items-center justify-center pr-10 pl-10'>
+              <Button>{'+'}</Button>
+              <h3>{'Hire staff'}</h3>
+            </div>
+            <div className='flex flex-col items-center justify-center pr-10 pl-10'>
+              <Button>{'+'}</Button>
+              <h3>{'Hire staff'}</h3>
+            </div>
+            <div className='flex flex-col items-center justify-center pr-10 pl-10'>
+              <Button>{'+'}</Button>
+              <h3>{'Hire staff'}</h3>
+            </div>
+            <div className='flex flex-col items-center justify-center pr-10 pl-10'>
+              <Button>{'+'}</Button>
+              <h3>{'Hire staff'}</h3>
+            </div>
           </div>
         </div>
       </div>
