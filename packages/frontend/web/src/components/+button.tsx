4a83ced600be6daf351f91ce12ca50ac557681a9
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-04-09 15:20:51 +0200

    création component bouton

---
+++ packages/frontend/web/src/components/+button.tsx
@@ -0,0 +1,21 @@
+import type { PropsWithChildren } from 'react';
+
+type ButtonProps = PropsWithChildren<{
+  readonly onClick?: () => void;
+}>;
+
+function Button({ onClick, children }: ButtonProps) {
+  return (
+    <div className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+      <button
+        type={'button'}
+        onClick={onClick}
+        className={`flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white`}
+      >
+        {children}
+      </button>
+    </div>
+  );
+}
+
+export default Button;
--- packages/frontend/web/src/pages/main-menu.tsx
+++ packages/frontend/web/src/pages/main-menu.tsx
@@ -1,12 +1,8 @@
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+
+import Button from '../components/+button';
 
 export default function MainMenu() {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    void navigate('/hireartist');
-  };
-
   return (
     <>
       <header className='h-19 text-center text-4xl'>
@@ -17,15 +13,9 @@ export default function MainMenu() {
           <h1 className='pb-7 text-6xl text-[#15414E] underline'>
             {'MyArtists'}
           </h1>
-          <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
-            <button
-              type='button'
-              onClick={handleClick}
-              className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
-            >
-              {'+'}
-            </button>
-          </span>
+          <Link to='/hireartist'>
+            <Button>{'+'}</Button>
+          </Link>
 
           <h2 className='pt-1 text-2xl text-[#15414E]'>
             {'Hire a new artist'}
@@ -36,30 +26,13 @@ export default function MainMenu() {
 
           <div className='flex h-50 items-center justify-center'>
             <div className='flex flex-col items-center justify-center'>
-              <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
-                <button
-                  type='button'
-                  onClick={handleClick}
-                  className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
-                >
-                  {'+'}
-                </button>
-              </span>
-
+              <Button>{'+'}</Button>
               <h2 className='pt-1 text-2xl text-[#15414E]'>
                 {'Create a new single'}
               </h2>
             </div>
             <div className='flex flex-col items-center justify-center pl-10'>
-              <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
-                <button
-                  type='button'
-                  onClick={handleClick}
-                  className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
-                >
-                  {'+'}
-                </button>
-              </span>
+              <Button>{'+'}</Button>
               <h2 className='pt-1 text-2xl text-[#15414E]'>
                 {'Create a new album'}
               </h2>
@@ -69,42 +42,10 @@ export default function MainMenu() {
         <div className='h-70 pt-7'>
           <h1 className='text-6xl text-[#15414E] underline'>{' Staff'}</h1>
           <div className='flex h-50 items-center justify-center'>
-            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
-              <button
-                type='button'
-                onClick={handleClick}
-                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
-              >
-                {'+'}
-              </button>
-            </span>
-            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
-              <button
-                type='button'
-                onClick={handleClick}
-                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
-              >
-                {'+'}
-              </button>
-            </span>
-            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
-              <button
-                type='button'
-                onClick={handleClick}
-                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
-              >
-                {'+'}
-              </button>
-            </span>
-            <span className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
-              <button
-                type='button'
-                onClick={handleClick}
-                className='flex h-11 flex-col items-center justify-center pb-3 text-6xl text-white'
-              >
-                {'+'}
-              </button>
-            </span>
+            <Button>{'+'}</Button>
+            <Button>{'+'}</Button>
+            <Button>{'+'}</Button>
+            <Button>{'+'}</Button>
           </div>
         </div>
       </div>
