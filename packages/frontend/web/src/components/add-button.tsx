3ee514e4f9002aed2722f4e2da7ea4b739429fa7
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-04-11 10:57:05 +0200

    modif add-button.tsx

--- packages/frontend/web/src/components/add-button.tsx
+++ packages/frontend/web/src/components/add-button.tsx
@@ -6,7 +6,7 @@ type ButtonProps = PropsWithChildren<{
 
 function AddButton({ onClick, children }: ButtonProps) {
   return (
-    <div className='mx-auto flex h-13 w-14 items-center justify-center rounded-xl bg-[#15414E] shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
+    <div className='bg-secondary mx-auto flex h-13 w-14 items-center justify-center rounded-xl shadow-[3px_5px_6px_rgba(0,0,0,0.30)] inset-ring-2 inset-ring-white'>
       <button
         type={'button'}
         onClick={onClick}
