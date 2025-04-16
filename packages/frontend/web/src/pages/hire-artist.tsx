f3a790e5bff2ce1e97b3c301ac549d81db2bc9c6
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-04-16 15:46:21 +0200

    fini

--- packages/frontend/web/src/pages/hire-artist.tsx
+++ packages/frontend/web/src/pages/hire-artist.tsx
@@ -1,11 +1,11 @@
-import { ArrowLeft, StarIcon } from 'lucide-react';
+import { ArrowLeft } from 'lucide-react';
 
 export default function HireArtist() {
   return (
     <div className='flex min-h-screen flex-col items-center bg-white px-4 py-6'>
       <div className='mb-4 flex w-full items-center justify-between'>
         <button className='text-secondary hover:text-orange-500'>
-          <ArrowLeft className='h-6 w-6' />
+          <ArrowLeft className='h-10 w-10' />
         </button>
         <h1 className='text-secondary text-center text-2xl font-bold underline underline-offset-4'>
           {'HIRE ARTISTS'}
@@ -17,7 +17,7 @@ export default function HireArtist() {
         {'ARTISTS'}
       </div>
 
-      <div className='bg-secondary mt-4 flex h-20 w-110 items-center justify-evenly rounded-sm text-white'>
+      <div className='bg-secondary mt-4 flex h-20 w-110 items-center justify-evenly rounded-sm text-white shadow-[3px_5px_6px_rgba(0,0,0,0.30)]'>
         <img
           className='h-16 w-16 rounded-4xl'
           src='/assets/alex-harper.jpeg'
@@ -32,7 +32,7 @@ export default function HireArtist() {
           <img className='h-6 w-6' src='/assets/star-sign.png' alt='' />
         </h2>
         <span className='flex flex-col items-center'>
-          <button className='flex h-8 w-18 items-center justify-center rounded-sm bg-orange-500 pl-2 text-xl font-bold'>
+          <button className='flex h-8 w-18 items-center justify-center rounded-sm bg-orange-500 pl-2 text-xl font-bold shadow-[3px_5px_6px_rgba(0,0,0,0.30)]'>
             {' Hire '}
             <img
               className='h-7 w-7'
@@ -50,6 +50,9 @@ export default function HireArtist() {
           </h2>
         </span>
       </div>
+      <button className='bg-secondary mt-5 flex h-8 w-25 items-center justify-center rounded-sm text-xl text-white shadow-[3px_5px_6px_rgba(0,0,0,0.30)]'>
+        {'See More'}
+      </button>
     </div>
   );
 }
