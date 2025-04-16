a311e891257e45dbef76a58dda0ed2af23210fa7
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-04-16 16:29:56 +0200

    header modify

--- packages/frontend/web/src/components/header.tsx
+++ packages/frontend/web/src/components/header.tsx
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import MenuBurger from '../../public/assets/burger-bar.png';
-import Close from '../../public/assets/croix.png';
+import Close from '../../public/assets/cross.png';
+import CustomLink from './customer-link';
 
 export default function Header() {
   const [isOpen, setIsOpen] = useState(false);
@@ -10,15 +11,15 @@ export default function Header() {
     setIsOpen(false);
   };
   return (
-    <header className='font-display border-b-orange bg-blue flex h-[105px] w-full flex-col border-b-3 text-white sm:h-[185px]'>
+    <header className='font-display border-b-orange bg-blue flex h-26 w-full flex-col border-b-3 text-white sm:h-46'>
       <div
-        className='slogan m-auto mt-[48px] hidden text-[48px] font-bold sm:block'
+        className='slogan m-auto mt-12 hidden text-5xl font-bold sm:block'
         onClick={closeMenu}
       >
         {'MUSIC LABEL MANAGER'}
       </div>
       <nav className='m-auto'>
-        <ul className='mb-[40px] hidden flex-row gap-[50px] sm:flex'>
+        <ul className='mb-10 hidden flex-row gap-12 sm:flex'>
           <li>
             <Link to=''>{'SIGN IN'}</Link>
           </li>
@@ -35,7 +36,7 @@ export default function Header() {
         onClick={closeMenu}
       />
       <div
-        className='absolute top-8 right-5 z-4 flex w-[32px] sm:hidden'
+        className='absolute top-8 right-5 z-4 flex w-8 sm:hidden'
         onClick={() => {
           setIsOpen(!isOpen);
         }}
@@ -44,27 +45,15 @@ export default function Header() {
       </div>
       <div className={isOpen ? 'flex' : 'hidden'} onClick={closeMenu} />
       <nav
-        className={`bg-blue absolute top-[69px] z-3 h-[247px] w-full items-start justify-center text-2xl shadow-[0_5px_5px_rgba(0,0,0,0.25)] ${isOpen ? 'flex' : 'hidden'}`}
+        className={`bg-blue absolute top-18 z-3 h-62 w-full items-start justify-center text-2xl shadow-[0_5px_5px_rgba(0,0,0,0.25)] ${isOpen ? 'flex' : 'hidden'}`}
       >
         <ul onClick={closeMenu} className='text-center'>
-          <li className='hover:text-orange'>
-            <Link to='/'>{'Home'}</Link>
-          </li>
-          <li className='hover:text-orange'>
-            <Link to=''>{'My Album'}</Link>
-          </li>
-          <li className='hover:text-orange'>
-            <Link to=''>{'My artists'}</Link>
-          </li>
-          <li className='hover:text-orange'>
-            <Link to=''>{'Privacy Policy'}</Link>
-          </li>
-          <li className='hover:text-orange'>
-            <Link to=''>{'Profil'}</Link>
-          </li>
-          <li className='hover:text-orange'>
-            <Link to=''>{'Log out'}</Link>
-          </li>
+          <CustomLink to='/'>{'Home'}</CustomLink>
+          <CustomLink to=''>{'My Album'}</CustomLink>
+          <CustomLink to=''>{'My artists'}</CustomLink>
+          <CustomLink to=''>{'Privacy Policy'}</CustomLink>
+          <CustomLink to=''>{'Profil'}</CustomLink>
+          <CustomLink to=''>{'Log out'}</CustomLink>
         </ul>
       </nav>
     </header>
