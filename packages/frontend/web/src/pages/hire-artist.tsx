83e0d753c866a2b2dca9d198a74fbef673fae9c6
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-04-17 16:22:59 +0200

    changement TAILLE image

--- packages/frontend/web/src/pages/hire-artist.tsx
+++ packages/frontend/web/src/pages/hire-artist.tsx
@@ -15,45 +15,50 @@ export default function HireArtist() {
         <div className='h-6 w-6' />
       </div>
 
-      <div className='mb-4 flex flex-col text-xl font-medium text-teal-800'>
+      <div className='mb-8 flex flex-col text-xl font-medium text-teal-800'>
         {'ARTISTS'}
       </div>
-
-      <div className='bg-secondary mt-4 flex h-20 w-110 items-center justify-evenly rounded-sm text-white shadow-[3px_5px_6px_rgba(0,0,0,0.30)]'>
-        <img
-          className='h-16 w-16 rounded-4xl'
-          src='/assets/alex-harper.jpeg'
-          alt=''
-        />
-        <span className='flex flex-col items-center'>
-          <h2 className='ml-2'>{' Alex Harper'}</h2>
-          <h3>{' Rock'}</h3>
-        </span>
-        <h2 className='ml-5 flex items-center font-bold'>
-          {'3,2 '}
-          <img className='h-6 w-6' src='/assets/star-sign.png' alt='' />
-        </h2>
-        <span className='flex flex-col items-center'>
-          <button
-            type='button'
-            className='flex h-8 w-18 items-center justify-center rounded-sm bg-orange-500 pl-2 text-xl font-bold shadow-[3px_5px_6px_rgba(0,0,0,0.30)]'
-          >
-            {' Hire '}
+      <div className='flex flex-col gap-4'>
+        <div className='bg-secondary flex h-20 w-110 items-center justify-evenly rounded-sm text-white shadow-[3px_5px_6px_rgba(0,0,0,0.30)]'>
+          <img
+            className='h-16 w-16 rounded-4xl'
+            src='/assets/alex-harper.jpeg'
+            alt=''
+          />
+          <span className='flex flex-col items-center'>
+            <h2 className='ml-2'>{' Alex Harper'}</h2>
+            <h3>{' Rock'}</h3>
+          </span>
+          <span className='flex items-center'>
+            <h2 className='flex items-center font-bold'>{'3,2 '}</h2>
             <img
-              className='h-7 w-7'
-              src='/assets/sign.png'
-              alt='contract logo'
-            />
-          </button>
-          <h2 className='flex items-center font-bold'>
-            {'50,000'}
-            <img
-              className='mt-1 h-4 w-4'
-              src='/assets/dollar-icon.png'
+              className='mt-0.5 h-5 w-5'
+              src='/assets/star-sign.png'
               alt=''
             />
-          </h2>
-        </span>
+          </span>
+          <span className='flex flex-col items-center'>
+            <button
+              type='button'
+              className='flex h-8 w-18 items-center justify-center rounded-sm bg-orange-500 pl-2 text-xl font-bold shadow-[3px_5px_6px_rgba(0,0,0,0.30)]'
+            >
+              {' Hire '}
+              <img
+                className='h-7 w-7'
+                src='/assets/sign.png'
+                alt='contract logo'
+              />
+            </button>
+            <span className='flex items-center'>
+              <h2 className='flex items-center font-bold'>{'50,000'}</h2>
+              <img
+                className='mt-0.5 h-3.5 w-3.5'
+                src='/assets/dollar-icon.png'
+                alt=''
+              />
+            </span>
+          </span>
+        </div>
       </div>
       <button
         type='button'
