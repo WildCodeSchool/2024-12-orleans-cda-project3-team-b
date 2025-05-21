e4789b69c110dc44cc151a793ec8896499428086
ticoq971 <138494189+ticoq971@users.noreply.github.com>
2025-05-21 13:54:47 +0200

    Update 20250418072203-update-bdd.ts

--- packages/backend/migrations/src/migrations/20250418072203-update-bdd.ts
+++ packages/backend/migrations/src/migrations/20250418072203-update-bdd.ts
@@ -86,6 +86,12 @@ export async function up(db: Kysely<DB>): Promise<void> {
     await sql`
     ALTER TABLE singles_marketing DROP albums_id;
     `.execute(trx);
+
+    // albums
+await sql`
+ALTER TABLE albums ADD notoriety_gain INT NOT NULL DEFAULT 0;
+`.execute(trx);
+
   });
 }
 
@@ -93,6 +99,12 @@ export async function down(db: Kysely<DB>): Promise<void> {
   // Migration code that reverts the database to the previous state.
   await db.transaction().execute(async (trx) => {
     //
+
+
+    await sql`
+ALTER TABLE albums DROP COLUMN notoriety_gain;
+`.execute(trx);
+
 
     await sql`
     ALTER TABLE singles_marketing ADD albums_id INT NOT NULL;
