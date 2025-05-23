819ef425a46facd81aa2fd338fd207a9bcb0ae9f
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-05-23 09:58:03 +0200

    delete pu-budget

--- packages/backend/api/src/games/index.ts
+++ packages/backend/api/src/games/index.ts
@@ -3,14 +3,12 @@ import express from 'express';
 import getLabelRouter from './get-labels';
 import getLogosRouter from './get-logos';
 import getLabelInfoRouter from './label-info';
-import postBuySomethinglRouter from './put-budget';
 import postRegisterLabelRouter from './register-label';
 
 const gamesRouter = express.Router();
 gamesRouter.use(getLogosRouter);
 gamesRouter.use(postRegisterLabelRouter);
 gamesRouter.use(getLabelRouter);
-gamesRouter.use(postBuySomethinglRouter);
 gamesRouter.use(getLabelInfoRouter);
 
 export default gamesRouter;
