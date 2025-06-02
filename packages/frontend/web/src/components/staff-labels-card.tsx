8630f30ae4ed6c5eb6f8ff9c171a4a28ac437f2a
Tristan aka Desperados <ticoq97.1@outlook.com>
2025-06-02 15:09:37 +0200

    reponsive main-menu et hire-artists et leurs composants

--- packages/frontend/web/src/components/staff-labels-card.tsx
+++ packages/frontend/web/src/components/staff-labels-card.tsx
@@ -17,9 +17,9 @@ export default function StaffLabelsCard({ staf }: StaffCardProps) {
   };
 
   return (
-    <div className='bg-secondary flex w-30 items-center justify-around rounded-sm text-white shadow-[3px_5px_6px_rgba(0,0,0,0.30)]'>
+    <div className='bg-secondary flex h-10 w-35 items-center justify-between gap-2 rounded-sm p-2 text-white shadow-[3px_5px_6px_rgba(0,0,0,0.30)] sm:h-12 sm:w-50 sm:flex-row sm:gap-0 md:w-50'>
       <img
-        className='h-10 w-10 rounded-4xl'
+        className='h-8 w-8 rounded-4xl md:h-10 md:w-10'
         src={`/assets/${staf.image}`}
         alt=''
       />
