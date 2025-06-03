4ebaeedb96f43c30d1eda22b8bb0454a921b4479
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-06-03 09:54:06 +0200

    fix view artists_hired and artists

--- packages/backend/api/src/artists-hired/artists-hired.ts
+++ packages/backend/api/src/artists-hired/artists-hired.ts
@@ -80,10 +80,25 @@ artistsHiredRouter.post('/', async (req: Request, res) => {
   }
 });
 
-artistsHiredRouter.get('/', async (req, res) => {
+artistsHiredRouter.get('/', async (req: Request, res) => {
+  const userId = req.userId;
+  if (userId === undefined) {
+    res.json({
+      ok: false,
+    });
+    return;
+  }
   try {
     const artistsHired = await db
-      .selectFrom('artists_hired')
+      .selectFrom('users')
+      .where('users.id', '=', userId)
+      .leftJoin('labels', 'labels.users_id', 'users.id')
+      .leftJoin('label_artists', 'label_artists.label_id', 'labels.id')
+      .leftJoin(
+        'artists_hired',
+        'artists_hired.id',
+        'label_artists.artists_hired_id',
+      )
       .leftJoin('artists', 'artists_hired.artists_id', 'artists.id')
       .leftJoin('milestones', 'artists_hired.milestones_id', 'milestones.id')
       .leftJoin('genres', 'artists.genres_id', 'genres.id')
@@ -100,7 +115,7 @@ artistsHiredRouter.get('/', async (req, res) => {
         'milestones.name as milestone_name',
         'genres.name as genre_name',
       ])
-      .where('artists.id', 'is', null)
+      .where('artists.id', 'is not', null)
       .execute();
 
     res.json(artistsHired);
