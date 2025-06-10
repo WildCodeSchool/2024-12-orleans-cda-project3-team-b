893d884551c9839254da584fc1fd9d507478687b
dykhounphypheth charles-eric <dykhounphypheth@gmail.com>
2025-06-10 15:25:47 +0200

    delete comments

--- packages/backend/api/src/artists/artists.ts
+++ packages/backend/api/src/artists/artists.ts
@@ -1,4 +1,3 @@
-// import express from 'express';
 import { type Request, Router } from 'express';
 
 import { db } from '@app/backend-shared';
--- packages/backend/api/src/games/get-staff.ts
+++ packages/backend/api/src/games/get-staff.ts
@@ -1,4 +1,3 @@
-// import express from 'express';
 import { type Request, Router } from 'express';
 
 import { db } from '@app/backend-shared';
@@ -6,58 +5,30 @@ import { db } from '@app/backend-shared';
 const getStaffRouter = Router();
 
 async function getStaff(userId: number) {
-  return (
-    db
-      .selectFrom('staff')
-      // .leftJoin('staff_label', 'staff_label.labels_id', 'labels.id')
-      // .leftJoin('staff','staff.id','staff_label.staff_id')
-      .select([
-        'staff.id',
-        'staff.job',
-        'staff.bonus',
-        'staff.price',
-        'staff.image',
-      ])
-      .where((eb) =>
-        eb.not(
-          eb.exists(
-            eb
-              .selectFrom('staff_label')
-              .leftJoin('labels', 'labels.id', 'staff_label.labels_id')
-              .select('staff_label.id')
-              .whereRef('staff_label.staff_id', '=', 'staff.id')
-              .where('labels.users_id', '=', userId),
-          ),
+  return db
+    .selectFrom('staff')
+    .select([
+      'staff.id',
+      'staff.job',
+      'staff.bonus',
+      'staff.price',
+      'staff.image',
+    ])
+    .where((eb) =>
+      eb.not(
+        eb.exists(
+          eb
+            .selectFrom('staff_label')
+            .leftJoin('labels', 'labels.id', 'staff_label.labels_id')
+            .select('staff_label.id')
+            .whereRef('staff_label.staff_id', '=', 'staff.id')
+            .where('labels.users_id', '=', userId),
         ),
-      )
-      .execute()
-  );
+      ),
+    )
+    .execute();
 }
 
-// getStaffRouter.get('/staff', async (req: Request, res) => {
-//   const userId = req.userId;
-//   if (userId === undefined) {
-//     res.json({
-//       ok: false,
-//     });
-//     return;
-//   }
-
-// async function getStaff() {
-//   return db
-
-//     .selectFrom('staff')
-//     .leftJoin('staff_label', 'staff_label.staff_id', 'staff.id')
-//     .select([
-//       'staff.id',
-//       'staff.job',
-//       'staff.bonus',
-//       'staff.price',
-//       'staff.image',
-//     ])
-//     .where('staff_label.staff_id', 'is', null)
-//     .execute();
-// }
 export type Staff = Awaited<ReturnType<typeof getStaff>>[number];
 
 getStaffRouter.get('/staff', async (req: Request, res) => {
